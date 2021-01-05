1ff3ffdbceb69a632b7a4f0ff35b594b8192cea6
Simon Steindl <kosimst@gmail.com>
2021-01-05 12:52:54 +0100

    Initial commit

---
+++ admin/words.js
@@ -0,0 +1,41 @@
+/*global systemDictionary:true */
+'use strict';
+
+systemDictionary = {
+    'smart-connect-firestore-sync adapter settings': {
+        en: 'Adapter settings for smart-connect-firestore-sync',
+        de: 'Adaptereinstellungen für smart-connect-firestore-sync',
+        ru: 'Настройки адаптера для smart-connect-firestore-sync',
+        pt: 'Configurações do adaptador para smart-connect-firestore-sync',
+        nl: 'Adapterinstellingen voor smart-connect-firestore-sync',
+        fr: "Paramètres d'adaptateur pour smart-connect-firestore-sync",
+        it: "Impostazioni dell'adattatore per smart-connect-firestore-sync",
+        es: 'Ajustes del adaptador para smart-connect-firestore-sync',
+        pl: 'Ustawienia adaptera dla smart-connect-firestore-sync',
+        'zh-cn': 'smart-connect-firestore-sync的适配器设置',
+    },
+    option1: {
+        en: 'option1',
+        de: 'Option 1',
+        ru: 'Опция 1',
+        pt: 'Opção 1',
+        nl: 'Optie 1',
+        fr: 'Option 1',
+        it: 'opzione 1',
+        es: 'Opción 1',
+        pl: 'opcja 1',
+        'zh-cn': '选项1',
+    },
+    option2: {
+        en: 'option2',
+        de: 'Option 2',
+        ru: 'option2',
+        pt: 'opção 2',
+        nl: 'Optie 2',
+        fr: 'Option 2',
+        it: 'opzione 2',
+        es: 'opcion 2',
+        pl: 'Opcja 2',
+        'zh-cn': '选项2',
+    },
+};
